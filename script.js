e800b8b842084a52234fe6693e5edf3faeeecfe4
unknown <katotodan@gmail.com>
2024-10-26 17:12:54 +0300

    some style added

--- script.js
+++ script.js
@@ -11,16 +11,26 @@ setInterval(() => {
     if(index === 3){
         index = 0
     }
-    headerPara.innerHTML = ''
-    setTimeout(() => {
-        headerPara.innerHTML = paragraphWord[index]
-        document.querySelector('#word').innerHTML = paragraphWord[index]
-    }, 500);
+    headerPara.innerHTML = paragraphWord[index]
+
 }, 4000);
 
 // Script for menu responsiveness
 const menu = document.querySelector('.menu-btn')
 const deletebtn = document.querySelector('.delete-btn')
+
+// Code for displaying and hiding the menu
+document.querySelector("body").addEventListener("click", (e)=>{
+    // Check if the event come from the menu or menu drowpdown or delete btn or something else
+    if(e.target.dataset.menu != "menu"){
+        // If there is that classlist then remove it
+        menu.classList.remove("active")
+    }else if(e.target.dataset.menu ==="menu"){
+        // Add the class list
+        menu.classList.toggle("active")
+    }
+    
+})
 
 
 
